5407f702554f040aef8afbdcd477d430220db933
omarsamir <omarsamsoliman@gmail.com>
2020-05-05 00:23:27 +0200

    feat: web sockets connect/disconnect/emit mesaages and events/broadcasts

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,21 @@
-console.log("CHAT SCRIPT")
-io()
+
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+// socket.on('countUpdated', (count) => {
+//     console.log(`The count has been updated! ${count}`)
+// })
+
+// document.querySelector('#increment').addEventListener('click' ,() => {
+//     console.log('clicked')
+//     socket.emit('increment')
+// })
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    // const messaage = document.querySelector('input').value
+    const messaage = e.target.elements.message.value
+    socket.emit('sendMessage',messaage)
+})
--- src/index.js
+++ src/index.js
@@ -13,8 +13,32 @@ const port  = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+// let count = 0
+
+// server (emit) -> client (recieve) - countUpdated
+// client (emit) -> server (recieve) - increment
+io.on('connection', (socket) => {
     console.log('New WebSoket connection')
+
+    io.emit('message', 'Welcome')
+
+    // send to all except this socket
+    socket.broadcast.emit('message','A new user has joined')
+
+    socket.on('sendMessage',(message) => {
+        io.emit('message',message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message','User A left')
+    })
+    // socket.emit("countUpdated",count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated',count)
+    //     io.emit('countUpdated',count) // to change immidiately
+    // })
 })
 
 server.listen(port,() => {
